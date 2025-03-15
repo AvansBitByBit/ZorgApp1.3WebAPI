ec39dcd6278739b77698b0a6114d2cb846adcdfd
Yazan26 <yazandawud75@gmail.com>
2025-03-15 16:52:11 +0100

    added dapper, removed http from launchsettings

--- ZorgWebApi/Program.cs
+++ ZorgWebApi/Program.cs
@@ -1,12 +1,25 @@
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Avans.Identity.Dapper;
+using Dapper;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+
+
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -14,6 +27,9 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
