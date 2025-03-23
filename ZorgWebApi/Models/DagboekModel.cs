96896719afe6889ddaf680de3e309791318a1fbb
Rishyan Syal <karanrishyan@gmail.com>
2025-03-24 00:56:38 +0100

    Updated DagboekModel Content --> Contents because conflict in database

--- ZorgWebApi/Models/DagboekModel.cs
+++ ZorgWebApi/Models/DagboekModel.cs
@@ -4,8 +4,8 @@
     {
         public int ID { get; set; }
         public string Title { get; set; }
-        public string Content { get; set; }
+        public string Contents { get; set; }
         public string Date { get; set; }
-        public string UserId { get; set; } 
+        public string UserId { get; set; }
     }
 }
