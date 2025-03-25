6c5c9e4d87e5beda40e884f4a733318a1f03b2dc
Rishyan Syal <karanrishyan@gmail.com>
2025-03-25 19:21:05 +0100

    Fix workflow #2?

--- ZorgWebApi/Interfaces/IAfspraakRepository.cs
+++ ZorgWebApi/Interfaces/IAfspraakRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace ZorgWebApi.Interfaces
+﻿using ZorgWebApi.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ZorgWebApi.Interfaces
 {
     public interface IAfspraakRepository
     {
+        Task<IEnumerable<AfspraakModel>> GetAfspraken(string userId);
+        Task<AfspraakModel> GetAfspraakById(int id, string userId);
+        Task CreateAfspraak(AfspraakModel afspraak);
+        Task DeleteAfspraak(int id, string userId);
     }
 }
